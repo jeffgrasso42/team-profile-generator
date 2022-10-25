41ebb17e1a0359f78e472f919a0a2280880e881c
JAG <jagrasso42@gmail.com>
2022-10-25 18:28:05 -0400

    added index.js file with npm depencies and file dependencies

---
+++ index.js
@@ -0,0 +1,18 @@
+// DEPENDENCIES
+// npm
+const inquirer = require('inquirer');
+const fs = require('fs');
+const path = require('path');
+// JS files
+const Employee = require('./Employee');
+const Manager = require('../lib/Manager');
+const Engineer = require('../lib/Engineer');
+const Intern = require('../lib/Intern');
+
+// ASSIGNMENT CODE
+
+// FUNCTIONS
+
+// USER INTERACTIONS
+
+// INITIALIZATION
