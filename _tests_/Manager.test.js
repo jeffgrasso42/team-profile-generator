2ed2e9ea99112a059393cc50cea6c4ef4f6b49cb
JAG <jagrasso42@gmail.com>
2022-10-25 18:04:31 -0400

    Added tests for Manager constructor

---
+++ _tests_/Manager.test.js
@@ -0,0 +1,36 @@
+// DEPENDENCIES
+const Manager = require('../lib/Manager');
+
+describe('Manager Class', () => {
+  describe('Initialization', () => {
+    it('should make an object with name, id, email, and officeNum properties', () => {
+      // Assign/Act
+      const manager = new Manager(
+        'John Doe',
+        '12345',
+        'johndoe@example.com',
+        '1'
+      );
+
+      expect(manager.name).toBe('John Doe');
+      expect(manager.id).toBe('12345');
+      expect(manager.email).toBe('johndoe@example.com');
+      expect(manager.officeNum).toBe('1');
+      expect(typeof manager).toBe('object');
+    });
+  });
+
+  describe('getRole', () => {
+    it('should return the role of the office manager', () => {
+      // Assign/Act
+      const manager = new Manager(
+        'John Doe',
+        '12345',
+        'johndoe@example.com',
+        '1'
+      );
+
+      expect(manager.getRole()).toBe('manager');
+    });
+  });
+});
